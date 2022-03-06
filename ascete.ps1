e68f18c6cf129d6c06e85eff5b5cc901b69d1763
JACK <bi.alive@outlook.fr>
2022-03-06 13:58:36 +0100

    Premiers fichiers + ascete

---
+++ ascete.ps1
@@ -0,0 +1,54 @@
+# compare two title
+[xml]$old_title = Get-Content ./ascete/ascete.xml -Encoding UTF8
+$old = $old_title.rss.channel.item.title[0]
+
+[xml]$new_title = (invoke-webrequest -uri https://ascete.org/feed).Content
+$new = $new_title.rss.channel.item.title[0]
+
+if ( $new -eq $old ) {
+echo 'Last title already exist'
+} else {
+Invoke-WebRequest -Uri "https://ascete.org/feed" -OutFile "./ascete/ascete.xml"
+[xml]$data = Get-Content ./ascete/ascete.xml -Encoding UTF8
+
+$title = $data.rss.channel.item.title[0]
+$link = $data.rss.channel.item.link[0]
+$description = $data.rss.channel.item.description[0]
+
+# git and create tag
+git config --local user.email "a-d-r-i@outlook.fr"
+git config --local user.name "A-d-r-i"
+git add .
+git commit -m "[Bot] Update Ascete"
+git push -f
+
+# post tweet
+$twitter = (Select-String -Path config.txt -Pattern "twitter=(.*)").Matches.Groups[1].Value
+if ( $twitter -eq "y" )
+{
+Install-Module PSTwitterAPI -Force
+Import-Module PSTwitterAPI
+$OAuthSettings = @{
+ApiKey = "$env:PST_KEY"
+ApiSecret = "$env:PST_KEY_SECRET"
+AccessToken = "$env:PST_TOKEN"
+AccessTokenSecret = "$env:PST_TOKEN_SECRET"
+}
+Set-TwitterOAuthSettings @OAuthSettings
+Send-TwitterStatuses_Update -status "Nouvel article de Ascete ! $title
+
+Lien : $link
+#ascete #news #biodiversity #orthoptere #entomology
+"
+}
+
+# send telegram notification
+Function Send-Telegram {
+Param([Parameter(Mandatory=$true)][String]$Message)
+$Telegramtoken = "$env:TELEGRAM"
+$Telegramchatid = "$env:CHAT_ID"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($Telegramtoken)/sendMessage?chat_id=$($Telegramchatid)&text=$($Message)"}
+
+Send-Telegram -Message "[BN] Nouvel article de Ascete : $title - $link"
+}
