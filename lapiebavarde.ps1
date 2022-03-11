e55b80e534c572b41038cbd2b166c07b55751e59
JACK <bi.alive@outlook.fr>
2022-03-11 15:05:07 +0100

    Ajout de La Pie Bavarde

---
+++ lapiebavarde.ps1
@@ -0,0 +1,24 @@
+# variables
+$id = "lapiebavarde"
+$name = "La Pie Bavarde"
+$accounts = ""
+$tags ="#lapiebavarde #news #revue #biodiversity #science #environnement"
+
+# compare two title
+[xml]$old_title = Get-Content ./$id/$id.xml -Encoding UTF8
+$old = $old_title.rss.channel.item.title[0]
+
+[xml]$new_title = (invoke-webrequest -Uri "https://www.la-pie-bavarde.com/blog-feed.xml").Content
+$new = $new_title.rss.channel.item.title[0]
+
+if ( $new -eq $old ) {
+echo 'Last title already exist'
+} else {
+Invoke-WebRequest -Uri "https://www.la-pie-bavarde.com/blog-feed.xml" -OutFile "./$id/$id.xml"
+[xml]$data = Get-Content ./$id/$id.xml -Encoding UTF8
+
+$title = $data.rss.channel.item.title[0]
+$link = $data.rss.channel.item.link[0]
+
+./end_push.ps1
+}
